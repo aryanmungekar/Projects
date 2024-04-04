8e969d0496051b6859cd34fba6d0e2bcde80e905
Aryan Mungekar <aryan.mungekar@gmail.com>
2024-04-05 00:22:24 +0530

    all bugs fixed

--- Face Recognization System/project-fr.py
+++ Face Recognization System/project-fr.py
@@ -1,4 +1,4 @@
-import face_recoginition
+import face_recognition
 import cv2
 import numpy as np
 import csv
@@ -7,17 +7,17 @@ from datetime import datetime
 
 video_capture = cv2.VideoCapture(0)
 
-gates_image = face_recognition.load_image_file(photos/gates.jpeg)
-gates_encoding = face_recognition.face_encoding(gates_image)[0]
+gates_image = face_recognition.load_image_file("photos/gates.jpeg")
+gates_encoding = face_recognition.face_encodings(gates_image)[0]
 
-ambani_image = face_recognition.load_image_file(photos/ambani.jpeg)
-ambani_encoding = face_recognition.face_encoding(ambani_image)[0]
+ambani_image = face_recognition.load_image_file("photos/ambani.jpeg")
+ambani_encoding = face_recognition.face_encodings(ambani_image)[0]
 
-elon_image = face_recognition.load_image_file(photos/elon.jpeg)
-elon_encoding = face_recognition.face_encoding(elon_image)[0]
+elon_image = face_recognition.load_image_file("photos/elon.jpeg")
+elon_encoding = face_recognition.face_encodings(elon_image)[0]
 
-jeff_image = face_recognition.load_image_file(photos/jeff.jpeg)
-jeff_encoding = face_recognition.face_encoding(jeff_image)[0]
+jeff_image = face_recognition.load_image_file("photos/jeff.jpeg")
+jeff_encoding = face_recognition.face_encodings(jeff_image)[0]
 
 known_face_encoding = [
 gates_encoding,
@@ -54,8 +54,8 @@ while True:
     small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
     rgb_small_frame = small_frame[:,:,::-1]
     if s:
-        face_locations = face_recoginition.face_locations(rgb_small_frame)
-        face_encodings = face_recoginition.face_encodings(rgb_small_frame,face_locations)
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
         face_names = []
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(known_face_encoding,face_encoding)
@@ -73,7 +73,7 @@ while True:
                     current_time = now.strftime("%H-%M-%S")
                     lnwriter.writerow([name,current_time])
     cv2.imshow("attendence system",frame)
-    if cv2.waitkey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 video_capture.release()
